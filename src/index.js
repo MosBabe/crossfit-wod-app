c02ba01708ce268e575b501e477480ac45aa7765
MosBabe <mosmankoc12@gmail.com>
2022-12-11 09:42:58 +0300

    Add swagger, add cache example with comments

--- src/index.js
+++ src/index.js
@@ -1,17 +1,27 @@
 // run `node index.js` in the terminal
 
 
-// In src/index.js 
-const express = require("express"); 
+// In src/index.js
+const express = require("express");
 const bodyParser = require("body-parser");
 const v1WorkoutRouter = require("./v1/routes/workoutRoutes");
+const { swaggerDocs: V1SwaggerDocs } = require("./v1/swagger");
 
-const app = express(); 
-const PORT = process.env.PORT || 3000; 
+// *** ADD *** Cache brings problems with it. After develop your api ,first observe the api and check whether api needs cache
+const apicache = require("apicache");
+
+
+const app = express();
+// *** ADD *** Cache brings problems with it. After develop your api ,first observe the api and check whether api needs cache
+const cache = apicache.middleware;
+const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
+// *** ADD *** Cache brings problems with it. After develop your api ,first observe the api and check whether api needs cache
+app.use(cache("2 minutes"));
 app.use("/api/v1/workouts", v1WorkoutRouter);
 
-app.listen(PORT, () => { 
-    console.log(`API is listening on port ${PORT}`); 
+app.listen(PORT, () => {
+  console.log(`API is listening on port ${PORT}`);
+  V1SwaggerDocs(app, PORT);
 });
--- src/v1/routes/workoutRoutes.js
+++ src/v1/routes/workoutRoutes.js
@@ -1,9 +1,64 @@
 const express = require("express");
+const apicache = require("apicache");
 const workoutController = require("../../controllers/workoutController");
 const recordController = require("../../controllers/recordController");
 
 const router = express.Router();
+const cache = apicache.middleware;
 
+/* AUTHENTICATION EXAMPLE FOR WORKOUT CREATION, JUST COACHS CAN CREATE WORKOUTS, SO IT IS JUST EXAMPLE, THERE IS NO MIDDLEWARE RIGHT NOW.
+const authenticate = require("../../middlewares/authenticate");
+const authorize = require("../../middlewares/authorize");
+router.post("/",authenticate,authorize, workoutController.createNewWorkout);
+*/
+
+//router with cache sample(in src/index.js we implement cache for all routes)
+//router.get("/", cache("2 minutes"), workoutController.getAllWorkouts);
+
+/**
+ * @openapi
+ * /api/v1/workouts:
+ *   get:
+ *     tags:
+ *       - Workouts
+ *     parameters:
+ *       - in: query
+ *         name: mode
+ *         schema:
+ *           type: string
+ *         description: The mode of a workout
+ *     responses:
+ *       200:
+ *         description: OK
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status:
+ *                   type: string
+ *                   example: OK
+ *                 data:
+ *                   type: array 
+ *                   items: 
+ *                     $ref: "#/components/schemas/Workout"
+ *       5XX:
+ *         description: FAILED
+ *         content:
+ *           application/json:
+ *             schema:
+ *               type: object
+ *               properties:
+ *                 status: 
+ *                   type: string
+ *                   example: FAILED
+ *                 data:
+ *                   type: object
+ *                   properties:
+ *                     error:
+ *                       type: string 
+ *                       example: "Some error message"
+ */
 router.get("/", workoutController.getAllWorkouts);
 
 router.get("/:workoutId", workoutController.getOneWorkout);
